675a8a388a90d874949de6345477d72820fc5dad
Vu Manh Thang <thang@zyyx.jp>
2017-07-08 18:36:23 +0900

    translate to vietnamese

--- chat/app.js
+++ chat/app.js
@@ -1,4 +1,5 @@
 var http = require("http");
+var translate = require('google-translate-api');
 
 // 1.モジュールオブジェクトの初期化
 var fs = require("fs");
@@ -39,6 +40,16 @@ io.sockets.on("connection", function (socket) {
       console.log("Got error: " + e.message);
     }); 
     //io.sockets.emit("publish", {value:data.value});
+  });
+
+  // メッセージ送信カスタムイベント
+  socket.on("publish-vietnamese", function (data) {
+    translate(data.value, {from: 'ja', to: 'vi'}).then(res => {
+      console.log(res.text);
+      io.sockets.emit("publish", {value:data.user + res.text});
+    }).catch(err => {
+      console.error(err);
+    });
   });
 
   // 接続終了組み込みイベント(接続元ユーザを削除し、他ユーザへ通知)
